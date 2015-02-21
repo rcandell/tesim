575fd5117f0de5640117b6d8a171e8d559d6131f
Rick Candell <rick.candell@nist.gov>
2015-02-21 11:49:39 -0500

    add visual studio solution

--- c/tesim_nist.h
+++ c/tesim_nist.h
@@ -1,11 +1,19 @@
+/* 
+
+* Author: Rick Candell
+* Organization: National Institute of Standards and Technology
+*               U.S. Department of Commerce
+* License: Public Domain
+
+*/
 
 #ifndef __TESIM_H__
 #define __TESIM_H__
 
-#define NIST_DEBUG_ON (true)
-#define NIST_DEBUG(x) do { \
-  if (NIST_DEBUG_ON) { std::cerr << x << std::endl; } \
-} while (0)
+//#define NIST_DEBUG_ON (true)
+//#define NIST_DEBUG(x) do { \
+//  if (NIST_DEBUG_ON) { std::cerr << x << std::endl; } \
+//} while (0)
 
 // Singleton class of the TE simulator
 // Calling convention:
@@ -20,12 +28,9 @@
 class TESimulator
 {		
     public:
-        static TESimulator& getInstance()
-        {
-            static TESimulator    instance; 
-            return instance;
-        }
-		
+        static TESimulator* getInstance();
+		virtual ~TESimulator();
+
 		// initialize the simulation
 		void initialize(const double ts_ode, const double ts_plant, const double ts_ctlr);
 		
@@ -33,24 +38,17 @@ class TESimulator
 		void increment();
 		
 		// set and get for xmeas
-		const double* get_xmeas() const;
+		const double* get_xmeas() const { return m_xmeas; };
 		void set_xmeas(const double* xmeas);
 		
 		// set and get for xmv
-		const double* get_xmv() const;
+		const double* get_xmv() const { return m_xmv; };
 		void set_xmv(const double* xmv);		
 		
     private:
         TESimulator() {}; 
         TESimulator(TESimulator const&);    // Singleton: Don't Implement
         void operator=(TESimulator const&); // Singleton: Don't implement
-
-        // C++ 11
-        // =======
-        // We can use the better technique of deleting the methods
-        // we don't want.
-        TESimulator(TESimulator const&)     = delete;
-        void operator=(TESimulator const&)  = delete;
 		
 		// run the plant one time step
 		void increment_plant();
@@ -67,6 +65,10 @@ class TESimulator
 		double 		m_ts_ctlr;			// time step for the controller
 		unsigned 	m_ode_per_tstep;
 		unsigned 	m_ode_per_tscan;
+		double*		m_xmeas;
+		double*		m_xmv;
+
+		static TESimulator*    instance; 
 		
 };
 
