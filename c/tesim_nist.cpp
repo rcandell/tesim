fb885d92c0f98864238aa80fe4efb6f014c44462
rcandell <rick.candell@nist.gov>
2015-02-21 11:49:39 -0500

    add visual studio solution

---
+++ c/tesim_nist.cpp
@@ -0,0 +1,50 @@
+
+#include "tesim_nist.h"
+
+TESimulator* TESimulator::instance = 0;
+
+TESimulator* TESimulator::getInstance()
+{
+	if (TESimulator::instance == 0)
+	{
+		TESimulator::instance = new TESimulator;
+	}
+    return TESimulator::instance;
+}
+
+TESimulator::~TESimulator()
+{
+	delete TESimulator::instance;
+	TESimulator::instance = 0;
+}
+
+// initialize the simulation
+void TESimulator::initialize(const double ts_ode, const double ts_plant, const double ts_ctlr)
+{
+	
+}
+
+// increment both plant and controller one time step
+void TESimulator::increment()
+{
+	
+}
+
+// run the plant one time step
+void TESimulator::increment_plant()
+{
+	
+}
+
+// run the controller one scan interval
+void TESimulator::increment_controller()
+{
+	
+}
+
+// validates that tstep is integer multiple of ode time step
+void TESimulator::valid_times(const double ts_plant, const double ts_ctlr) throw (...)
+{
+	
+}
+
