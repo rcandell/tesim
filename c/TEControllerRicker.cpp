c41a4308c6184f8bcbc8ffeb921bb4a9a7e4b1de
rcandell <rick.candell@nist.gov>
2015-02-27 17:04:50 -0500

    added skeleton classes for Ricker cotnroller design

---
+++ c/TEControllerRicker.cpp
@@ -0,0 +1,10 @@
+/*
+
+* Author: Rick Candell (rick.candell@nist.gov)
+* Organization: National Institute of Standards and Technology
+*               U.S. Department of Commerce
+* License: Public Domain
+
+*/
+
+#include "TEControllerRicker.h"
