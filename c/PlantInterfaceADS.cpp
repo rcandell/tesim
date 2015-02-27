be968d3070143800a38288e1be527bfb7de67dc4
Rick Candell <rick.candell@nist.gov>
2015-02-26 20:08:48 -0500

    created classes for various interfaces

---
+++ c/PlantInterfaceADS.cpp
@@ -0,0 +1,11 @@
+/*
+
+* Author: Rick Candell (rick.candell@nist.gov)
+* Organization: National Institute of Standards and Technology
+*               U.S. Department of Commerce
+* License: Public Domain
+
+*/
+
+#include "PlantInterfaceADS.h"
+
---
+++ c/PlantInterfaceTCP.h
@@ -0,0 +1,20 @@
+/*
+
+* Author: Rick Candell (rick.candell@nist.gov)
+* Organization: National Institute of Standards and Technology
+*               U.S. Department of Commerce
+* License: Public Domain
+
+*/
+
+#ifndef __PLANTIFTCP_H__
+#define __PLANTIFTCP_H__
+
+#include "PlantInterface.h"
+
+class PlantInterfaceTCP : public PlantInterface
+{
+
+};
+
+#endif
