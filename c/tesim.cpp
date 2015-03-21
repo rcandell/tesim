46eb870e3b1a533e862489b02d40f2f78944e87e
Rick Candell <rick.candell@nist.gov>
2015-03-21 19:35:41 -0400

    remove dat files.  add main line args

--- c/tesim.cpp
+++ c/tesim.cpp
@@ -15,10 +15,27 @@
 #include <fstream>
 #include <iostream>
 
-dbl last_tMod = 0;
+void print_sim_params(double tstep, double tscan, int nsteps, double simtime)
+{
+	std::cout << "TE simulation" << std::endl;
+	std::cout << "simulation time: " << simtime << std::endl;
+	std::cout << "plant dt: " << tstep << std::endl;
+	std::cout << "ctlr dt: " << tscan << std::endl;
+	std::cout << "time steps: " << nsteps << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
+	double simtime = 0.0;
+	if (argc < 2)
+	{
+		printf("usage error.  simulation duration not specified");
+		exit(0);
+	}
+	else
+	{
+		simtime = atof(argv[1]);
+	}
 	TEPlant* teplant = TEPlant::getInstance();
 	TEController* tectlr = TEController::getInstance();
 
@@ -28,11 +45,15 @@ int main(int argc, char* argv[])
 	plant_log.open("teplant.dat");
 	ctlr_log.open("tectlr.dat");
 
-	int nsteps = 100; // 72 * 2000;
+	// simulation parameters
 	double t, tstep, tscan;
 	double *xmeas, *xmv;
 	t = 0;
 	tstep = 0.0005;
+	tscan = tstep;
+	int nsteps = int(simtime/tstep);
+	print_sim_params(tstep, tscan, nsteps, simtime);
+
 
 	// init the plant
 	teplant->initialize();
@@ -40,7 +61,6 @@ int main(int argc, char* argv[])
 	plant_log << t << "\t" << *teplant << std::endl;
 
 	// init the controller
-	tscan = tstep;
 	tectlr->initialize(tstep, tscan);
 	xmv = (double*)(tectlr->get_xmv());
 	ctlr_log << t << "\t" << *tectlr << std::endl;
