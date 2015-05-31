530c591bf352627c69fc9aa23dd5b6f60ee90142
rcandell <rick.candell@nist.gov>
2015-05-30 21:07:10 -0400

    add program options to top exec

--- c/run_tesims/run_tesims.cpp
+++ c/run_tesims/run_tesims.cpp
@@ -10,6 +10,9 @@
 */
 
 #include <boost/random.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/program_options.hpp>
+
 #include <string>
 #include <sstream>
 #include <cstdlib>
@@ -21,6 +24,9 @@ int tc_all_mc(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
+	// auto timer used as a performance profiler
+	boost::timer::auto_cpu_timer t_wall_auto;
+
 	//return tc_xmeas_sweep_noxmv(argc, argv);
 	//return tc_xmv_sweep_noxmeas(argc, argv);
 	return tc_all_mc(argc, argv);
@@ -28,6 +34,36 @@ int main(int argc, char* argv[])
 
 int tc_all_mc(int argc, char* argv[])
 {
+	unsigned num_iterations = 0;
+	std::string logfile_prefix;
+
+	// program options
+	namespace po = boost::program_options;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "print the help message")
+		("num-iterations,n", po::value<unsigned>(&num_iterations)->required(), "set the number of iterations for the simulation.")
+		("output-prefix,p", po::value<std::string>(&logfile_prefix)->required(), "set the prefix for the output files.")
+		;
+
+	po::variables_map vm;
+	try	{
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		if (vm.count("help"))
+		{
+			std::cout << desc << std::endl;
+			return 0;
+		}
+
+		po::notify(vm);
+	}
+	catch (po::error& e) {
+		std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+		std::cerr << desc << std::endl;
+		return 0;
+	}
+	po::notify(vm);
+
 	const std::string exec("..\\debug\\tesim");
 	const double simtime = 32;
 	const double tstep = 0.0005;
@@ -35,22 +71,22 @@ int tc_all_mc(int argc, char* argv[])
 	const unsigned ksave = 20;
 
 	// monte carlo parameters
-	unsigned N = 100;
+	unsigned seed = 1731;
 
-	std::string logfile_prefix = "mc_20150530";
-
-	boost::mt19937 rng_p;
+	boost::mt19937 rng_p(seed); 
 	boost::uniform_real<float> u_p(0.6F, 1.0F);
 	boost::variate_generator<boost::mt19937&, boost::uniform_real<float> > gen_p(rng_p, u_p);
-	boost::mt19937 rng_q;
+
+	boost::mt19937 rng_q(seed);
 	boost::uniform_real<float> u_q(0.05F, 0.5F);
 	boost::variate_generator<boost::mt19937&, boost::uniform_real<float> > gen_q(rng_q, u_q);
 
 	unsigned ii = 0;
-	while (ii < N)
+	while (ii < num_iterations)
 	{
 		float xmeas_p = gen_p();
 		float xmeas_q = gen_q();
+
 		float xmv_p = xmeas_p; // gen_p();
 		float xmv_q = xmeas_q; // gen_q();
 
