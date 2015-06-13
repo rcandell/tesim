46ef8d8fbee8829ce154cb34e1bf19c266b5d10b
rcandellnist <rick.candell@nist.gov>
2015-06-13 00:08:22 -0400

    fix shutdown logging issue

--- c/data/calculate_kpis.R
+++ c/data/calculate_kpis.R
@@ -2,7 +2,7 @@
 calc_vitae<-function(v, sp, tm )
 {
   deltaT<-diff(tm,1)
-  deltaT[length(tm)] = deltaT[length(tm)-1]
+  deltaT[[length(deltaT)]]<-deltaT[[length(deltaT)-1]]
   errs<-sp-v
   itae<-cumsum(tm*abs(errs)*deltaT)
   itae
@@ -11,7 +11,7 @@ calc_vitae<-function(v, sp, tm )
 calc_itae<-function(v, sp, tm )
 {
   deltaT<-diff(tm,1)
-  deltaT[length(tm)] = deltaT[length(tm)-1]
+  deltaT[[length(deltaT)]]<-deltaT[[length(deltaT)-1]]
   errs<-sp-v
   itae<-sum(tm*abs(errs)*deltaT)
   itae
@@ -20,7 +20,7 @@ calc_itae<-function(v, sp, tm )
 calc_iae<-function(v, tm, sp )
 {
   deltaT<-diff(tm,1)
-  deltaT[length(tm)] = deltaT[length(tm)-1]
+  deltaT[[length(deltaT)]]<-deltaT[[length(deltaT)-1]]
   errs<-sp-v
   iae<-sum(abs(errs)*deltaT)
   iae
@@ -29,55 +29,21 @@ calc_iae<-function(v, tm, sp )
 calc_msae<-function(v, tm, sp )
 {
   deltaT<-diff(tm,1)
-  deltaT[length(tm)] = deltaT[length(tm)-1]
+  deltaT[[length(deltaT)]]<-deltaT[[length(deltaT)-1]]
   errs<-sp-v
   sum(abs(errs)*deltaT)
 }
 
-# calc_ae<-function(v, tm, sp )
-# {
-#   errs<-sp-v
-#   errs
-# }
-# 
-# calc_sdae<-function(v, tm, sp )
-# {
-#   errs<-sp-v
-#   sd(errs)
-# }
-# 
-# calc_uae<-function(v, tm, sp )
-# {
-#   errs<-sp-v
-#   mean(errs)
-# }
-# 
-# calc_varae<-function(v, tm, sp )
-# {
-#   errs<-sp-v
-#   var(errs)
-# }
-
 kpidf<-function(df, tm, sp)
 {
   kpis<-sp
-#   NN<-length(sp$sp_name)
-#   kpis$msae = c(1:NN)
-#   kpis$iae = c(1:NN)
-#   kpis$itae = c(1:NN)
-#   kpis$uae = c(1:NN)
-#   kpis$varae = c(1:NN)
-#   kpis$sdae = c(1:NN)
-#   kpis$maxae = c(1:NN)
-#   kpis$minae = c(1:NN)
-#   kpis$medae = c(1:NN)
 
   for(nn in sp$sp_name)
   {
     v<-df[nn]
-    kpis$msae[sp_name==nn]   <-  calc_msae   (v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
-    kpis$iae[sp_name==nn]    <-  calc_msae   (v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
-    kpis$itae[sp_name==nn]   <-  calc_itae  (v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
+    kpis$msae[sp_name==nn]   <-  calc_msae(v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
+    kpis$iae[sp_name==nn]    <-  calc_msae(v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
+    kpis$itae[sp_name==nn]   <-  calc_itae(v=v, tm = tm, sp = sp[sp_name==nn,]$sp_value)
     kpis$uae[sp_name==nn]    <-  sapply(v-sp[sp_name==nn,]$sp_value, mean)
     kpis$varae[sp_name==nn]  <-  sapply(v-sp[sp_name==nn,]$sp_value, var)
     kpis$sdae[sp_name==nn]   <-  sapply(v-sp[sp_name==nn,]$sp_value, sd)
