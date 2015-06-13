8735bbc6074c7011f19b3f31f7e7916376382fd3
rcandell <rick.candell@nist.gov>
2015-06-13 17:17:41 -0400

    improvements to baseline plots and kpi calculations

--- c/data/analyze_plant_idv.R
+++ c/data/analyze_plant_idv.R
@@ -9,12 +9,15 @@ setwd("C:/Users/rnc4/git/TEsim/C/data")
 # the indexes for selected process variables
 source("common.R")
 
+plot_titles<-data.frame(n=names(base.df[xmeas_idx]),l=xmeas_labels)
+
 # load the baseline data
 base.df <- read.table("nochan/allidv_plant.dat", header = TRUE, sep = "\t", fill = TRUE, check.names = TRUE)
 
 # plot histograms for reactor pressure versus idv
+i="Reactor.Pressure.kPa"
 ggplot(data = base.df) +
-  geom_histogram(aes(x=Reactor.Pressure.kPa), color="black", fill="white")  +
+  geom_histogram(aes(x=Reactor.Pressure.kPa, y=..density..), color="black", fill="white")  +
   ggtitle(plot_titles[plot_titles$n==i,]$l) +
   geom_density(aes(x=Reactor.Pressure.kPa)) + 
   facet_wrap(~IDV, ncol = 2, scales="free") +
@@ -27,10 +30,22 @@ ggplot(data = base.df) +
   facet_wrap(~IDV, ncol = 3, scales="free") +
   ylab(" ") + xlab("time (hrs)")
 
-
-# plot box plots for reactor pressure versus idv
+###  BOXES PLOTS 
+## plot box plots for reactor pressure versus idv
 ggplot(data = base.df) +
   geom_boxplot(aes(x=time, y=Reactor.Pressure.kPa))  +
   ggtitle(plot_titles[plot_titles$n==i,]$l) +
   facet_grid(~IDV, scales="free") 
 
+## Stripper level
+i<-"Stripper.Underflow.m3.hr"
+ggplot(data = base.df) +
+  geom_boxplot(aes(x=time, y=Stripper.Underflow.m3.hr))  +
+  ggtitle(plot_titles[plot_titles$n==i,]$l) +
+  facet_grid(~IDV, scales="free") 
+
+i<-"Purge.Rate.kscmh"
+ggplot(data = base.df) +
+  geom_boxplot(aes(x=time, y=Purge.Rate.kscmh))  +
+  ggtitle("Purge Flow Rate") +
+  facet_grid(~IDV, scales="free") 
