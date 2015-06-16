0417a0d52c1d3fafd138567d8cefed26f8d81ebc
rcandellnist <rick.candell@nist.gov>
2015-06-16 11:56:25 -0400

    optimize calculation of KPI's for selected measured variables

--- c/data/calc_kpis_ricker_idv.R
+++ c/data/calc_kpis_ricker_idv.R
@@ -6,23 +6,27 @@ source("calc_kpis_lib.R")
 
 base.df <- read.table("nochan/allidv_plant.dat", header = TRUE, sep = "\t", fill = TRUE, check.names = TRUE)
 
-for (idvs in 2**c(0:14))
-{
-  print(" ")
-  print(" ")
-  print(paste0("*******  IDV # ", log2(idvs)," *******"))
-  base.time <- base.df[base.df$IDV==idvs,]$time
-  base.xmeas <- base.df[base.df$IDV==idvs, xmeas_idx]
-  base.xmeas <- base.xmeas[complete.cases(base.xmeas),]
-  
-  # compares set points with process variable and computes KPI's
-  sp_name<-names(base.xmeas)
-  sp_value<-c(2800,65,122.9,50,15,22.89,53.8,0.0)
-  sp<-data.frame(sp_name,sp_value)
-  base.kpis<-kpidf(df = base.xmeas, sp=sp, tm=base.time)
-  print(format(base.kpis, digits=2))
-  
-  # save the kpi's to file
-  fname<-paste0("ricker_idv_",log2(idvs),"_kpis.txt")
-  write.table(format(base.kpis, digits=2), fname, sep="\t", row.names = F)
-}
+base.time <- base.df$time
+base.xmeas <- base.df[, xmeas_idx]
+base.xmeas <- base.xmeas[complete.cases(base.xmeas),]
+
+# compares set points with process variable and computes KPI's
+sp_name<-names(base.xmeas)
+sp_value<-c(2800,65,122.9,50,15,22.89,53.8,0.0)
+sp<-data.frame(sp_name,sp_value)
+
+idvs <- 2**c(0:14)
+kpis <- lapply( idvs, 
+                FUN = function(ii) {
+                  df <- kpidf(df = base.df[base.df$IDV==ii, xmeas_idx], sp=sp, tm=base.time)
+                  df$IDV<-ii
+                  df
+                } )
+print(kpis)
+kpis <- do.call("rbind", kpis)
+
+fname<-"ricker_allidvs_kpis.txt"
+write.table(format(kpis, digits=2), fname, sep="\t", row.names = F)
+
+kpis[sp_name=="Reactor.Pressure.kPa",]
+kpis[sp_name=="Hourly.Cost",]
